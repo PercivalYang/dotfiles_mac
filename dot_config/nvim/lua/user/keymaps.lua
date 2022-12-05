38a0308987294da2cb88f2479d97887e867838bf
PercivalYang <yzy1998719@163.com>
2022-12-05 22:11:53 +0800

    Add markdown preview keymap and relativenumber in options

--- dot_config/nvim/lua/user/keymaps.lua
+++ dot_config/nvim/lua/user/keymaps.lua
@@ -83,3 +83,6 @@ keymap("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", opts)
 keymap("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", opts)
 keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
 keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
+
+-- Markdown Preview
+keymap("n", "<leader>md", ":MarkdownPreview<cr>", opts)
